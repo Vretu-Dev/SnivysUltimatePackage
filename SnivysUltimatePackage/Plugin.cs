2c1f7376c245f13f6ffdd1c6cacf402952e41fc1
Snivy Films <mill385@yahoo.com>
2024-12-24 14:02:54 -0500

    Version Bump

--- SnivysUltimatePackage/Plugin.cs
+++ SnivysUltimatePackage/Plugin.cs
@@ -24,7 +24,7 @@ namespace SnivysUltimatePackage
         public override string Name { get; } = "Snivy's Ultimate Plugin Package";
         public override string Author { get; } = "Vicious Vikki";
         public override string Prefix { get; } = "VVUltimatePluginPackage";
-        public override Version Version { get; } = new Version(1, 5, 1);
+        public override Version Version { get; } = new Version(1, 5, 2);
         public override Version RequiredExiledVersion { get; } = new Version(9, 0, 1);
         public static int ActiveEvent = 0;
         
