79b2d42b7d63c81d175ff82444b089d4908ab84d
Snivy Films <mill385@yahoo.com>
2025-01-08 15:21:57 -0500

    SSSS Item Info

--- SnivysUltimatePackage/Ssss.cs
+++ SnivysUltimatePackage/Ssss.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Text;
+using Exiled.API.Features.Items;
+using Exiled.CustomItems.API.Features;
 using Exiled.CustomRoles.API.Features;
 using NorthwoodLib.Pools;
-using SnivysUltimatePackage.Custom.Abilities;
+using SnivysUltimatePackage.Custom.Items.Armor;
+using SnivysUltimatePackage.Custom.Items.Firearms;
+using SnivysUltimatePackage.Custom.Items.Grenades;
+using SnivysUltimatePackage.Custom.Items.Injections;
+using SnivysUltimatePackage.Custom.Items.Other;
 using SnivysUltimatePackage.Custom.Roles;
 using UnityEngine;
 using UserSettings.ServerSpecific;
@@ -58,10 +64,39 @@ namespace SnivysUltimatePackage
                 }
             }
 
-            // Add the collected abilities to the settings
             settings.Add(new SSGroupHeader("VV Custom Roles Info"));
             settings.Add(new SSTextArea(null, StringBuilderPool.Shared.ToStringReturn(customRoleStringBuilder), SSTextArea.FoldoutMode.CollapsedByDefault));
 
+            StringBuilder customItemStringBuilder = StringBuilderPool.Shared.Rent();
+            var customItems = new List<CustomItem>
+            {
+                (CustomItem)ExplosiveResistantArmor.Get(typeof(ExplosiveResistantArmor)),
+                (CustomItem)ExplosiveRoundRevolver.Get(typeof(ExplosiveRoundRevolver)),
+                (CustomItem)MediGun.Get(typeof(MediGun)),
+                (CustomItem)Tranquilizer.Get(typeof(Tranquilizer)),
+                (CustomItem)C4.Get(typeof(C4)),
+                (CustomItem)EmpGrenade.Get(typeof(EmpGrenade)),
+                (CustomItem)NerveAgentGrenade.Get(typeof(NerveAgentGrenade)),
+                (CustomItem)SmokeGrenade.Get(typeof(SmokeGrenade)),
+                (CustomItem)DeadringerSyringe.Get(typeof(DeadringerSyringe)),
+                (CustomItem)KySyringe.Get(typeof(KySyringe)),
+                (CustomItem)AntiScp096Pills.Get(typeof(AntiScp096Pills)),
+                (CustomItem)InfinitePills.Get(typeof(InfinitePills)),
+                (CustomItem)PhantomLantern.Get(typeof(PhantomLantern)),
+                (CustomItem)Scp1499.Get(typeof(Scp1499)),
+            };
+            
+            foreach (var item in customItems)
+            {
+                if (item == null) continue;
+
+                customItemStringBuilder.AppendLine($"Item: {item.Name}");
+                customItemStringBuilder.AppendLine($"- Description: {item.Description}");
+            }
+            
+            settings.Add(new SSGroupHeader("VV Custom Items Info"));
+            settings.Add(new SSTextArea(null, StringBuilderPool.Shared.ToStringReturn(customItemStringBuilder), SSTextArea.FoldoutMode.CollapsedByDefault));
+            
             settings.Add(new SSGroupHeader("VV Custom Abilities Key Activators"));
             settings.Add(new SSKeybindSetting(Plugin.Instance.Config.SsssConfig.ActiveCamoId, "Active Camo", KeyCode.B, true, null));
             settings.Add(new SSKeybindSetting(Plugin.Instance.Config.SsssConfig.ChargeId, "Charge", KeyCode.B, true, null));
