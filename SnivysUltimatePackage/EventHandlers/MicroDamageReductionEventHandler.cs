44cf1047440e2823a2b96e23f73c5d816e6aacf1
Snivy Films <Snivy.Films@gmail.com>
2025-01-26 09:13:38 -0500

    Fixes maybe?

--- SnivysUltimatePackage/EventHandlers/MicroDamageReductionEventHandler.cs
+++ SnivysUltimatePackage/EventHandlers/MicroDamageReductionEventHandler.cs
@@ -14,10 +14,14 @@ namespace SnivysUltimatePackage.EventHandlers
             Log.Debug("VVUP Micro Damage Reduction: Checking if Micro Damage Reduction is enabled");
             if (!Plugin.Instance.Config.MicroDamageReductionConfig.IsEnabled)
                 return;
+            if (ev.Player == null)
+                return;
+            
             Log.Debug("VVUP Micro Damage Reduction: Checking if damage can be reduced");
-            if (ev.Attacker != ev.Player && ev.Attacker != null && ev.Player != null && ev.DamageHandler.Type == DamageType.MicroHid &&
+            if (ev.Attacker != ev.Player && ev.DamageHandler.Type == DamageType.MicroHid &&
                 Plugin.Instance.Config.MicroDamageReductionConfig.ScpDamageReduction.Contains(ev.Player.Role))
             {
+                if ()
                 ev.Amount /= Plugin.Instance.Config.MicroDamageReductionConfig.ScpDamageReductionValue;
                 Log.Debug($"VVUP Micro Damage Reduction: {ev.Player.Nickname} is {ev.Player.Role}, reducing damage");
             }
