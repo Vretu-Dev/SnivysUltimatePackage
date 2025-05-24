385222256c71e52a41eb895dc9c47908ca657637
Snivy Films <Snivy.Films@gmail.com>
2025-05-24 09:35:50 -0400

    Custom Keycards Forreals

--- SnivysUltimatePackage/Custom/Items/Keycards/OcfPrototypeKeycardRefined.cs
+++ SnivysUltimatePackage/Custom/Items/Keycards/OcfPrototypeKeycardRefined.cs
@@ -1,19 +1,29 @@
+using System;
+using System.Collections.Generic;
 using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Items;
+using Exiled.API.Features.Pickups;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Map;
+using Exiled.Events.EventArgs.Player;
+using Interactables.Interobjects.DoorUtils;
+using InventorySystem;
+using InventorySystem.Items.Keycards;
+using JetBrains.Annotations;
 using UnityEngine;
 using YamlDotNet.Serialization;
 using OperationCrossFire = SnivysUltimatePackage.EventHandlers.ServerEventsEventHandlers.OperationCrossfireEventHandlers;
+using Random = System.Random;
 
 namespace SnivysUltimatePackage.Custom.Items.Keycards
 {
-    [CustomItem(ItemType.KeycardFacilityManager)]
+    [CustomItem(ItemType.KeycardCustomManagement)]
     public class OcfPrototypeKeycardRefined : CustomItem
     {
         [YamlIgnore] 
-        public override ItemType Type { get; set; } = ItemType.KeycardFacilityManager;
+        public override ItemType Type { get; set; } = ItemType.KeycardCustomManagement;
         public override uint Id { get; set; } = 45;
         public override string Name { get; set; } = "Prototype Keycard Refined";
 
@@ -22,6 +32,90 @@ namespace SnivysUltimatePackage.Custom.Items.Keycards
 
         public override float Weight { get; set; } = 0.5f;
         public override SpawnProperties SpawnProperties { get; set; }
+        
+        [CanBeNull]
+        public static List<string> KeycardNames { get; set; } = new List<string>
+        {
+            "Dr. Vicious Vikki",
+        };
+        public static KeycardLevels KeycardPermissions { get; set; } = new KeycardLevels(1, 2, 3);
+        public static Color32 KeycardPermissionsColor { get; set; } = new Color32(0, 0, 0, 255);
+        public static Color32 KeycardPrimaryColor { get; set; } = new Color32(255, 0, 255, 255);
+        public static string KeycardLabel { get; set; } = "Prototype Keycard Refined";
+        public static Color32 KeycardLabelColor { get; set; } = new Color32(255, 255, 255, 255);
+
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+
+            Exiled.Events.Handlers.Map.PickupAdded += OnPickupAdded;
+            Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
+        }
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+
+            Exiled.Events.Handlers.Map.PickupAdded -= OnPickupAdded;
+            Exiled.Events.Handlers.Player.ItemAdded -= OnItemAdded;
+        }
+
+        private void OnPickupAdded(PickupAddedEventArgs ev) 
+        {
+            if (!Check(ev.Pickup))
+                return;
+
+            UpdateCard(ev.Pickup); 
+        }
+
+        private void OnItemAdded(ItemAddedEventArgs ev)
+        {
+            if (!Check(ev.Pickup))
+                return;
+
+            UpdateCard(ev.Pickup);
+        }
+
+        public static string KeycardName => GetRandomKeycardName();
+        private static string GetRandomKeycardName()
+        {
+            Random random = new Random();
+            if (KeycardNames == null || KeycardNames.Count == 0)
+            {
+                return "Someone decided to remove the keycard names.";
+            }
+            return KeycardNames[random.Next(KeycardNames.Count)];
+        }
+        
+        private void UpdateCard(Pickup pickup) //code taken from KeycardItem (kinda)
+        {
+            if (!Type.TryGetTemplate<KeycardItem>(out var item))
+                throw new ArgumentException("Template for itemType not found");
+
+            if (!item.Customizable)
+                return;
+
+            int num = 0;
+            DetailBase[] details = item.Details;
+
+            object[] args = new object[]
+            {
+                KeycardName,
+                KeycardPermissions,
+                KeycardPermissionsColor,
+                KeycardPrimaryColor,
+                KeycardLabel,
+                KeycardLabelColor
+            };
+
+            for (int i = 0; i < details.Length; i++)
+            {
+                if (details[i] is ICustomizableDetail customizableDetail)
+                {
+                    customizableDetail.SetArguments(new ArraySegment<object>(args, num, customizableDetail.CustomizablePropertiesAmount));
+                    num += customizableDetail.CustomizablePropertiesAmount;
+                }
+            }
+        }
 
         protected override void OnAcquired(Player player, Item item, bool displayMessage)
         {
--- SnivysUltimatePackageOneConfig/Custom/Items/Keycards/OcfPrototypeKeycardBasic.cs
+++ SnivysUltimatePackageOneConfig/Custom/Items/Keycards/OcfPrototypeKeycardBasic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Exiled.API.Enums;
 using Exiled.API.Features.Attributes;
@@ -5,17 +6,23 @@ using Exiled.API.Features.Pickups;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.EventArgs;
 using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Map;
+using Exiled.Events.EventArgs.Player;
+using Interactables.Interobjects.DoorUtils;
+using InventorySystem;
+using InventorySystem.Items.Keycards;
+using JetBrains.Annotations;
 using UnityEngine;
 using YamlDotNet.Serialization;
 using OperationCrossFire = SnivysUltimatePackageOneConfig.EventHandlers.ServerEventsEventHandlers.OperationCrossfireEventHandlers;
 
 namespace SnivysUltimatePackageOneConfig.Custom.Items.Keycards
 {
-    [CustomItem(ItemType.KeycardJanitor)]
+    [CustomItem(ItemType.KeycardCustomSite02)]
     public class OcfPrototypeKeycardBasic : CustomItem
     {
         [YamlIgnore] 
-        public override ItemType Type { get; set; } = ItemType.KeycardJanitor;
+        public override ItemType Type { get; set; } = ItemType.KeycardCustomSite02;
         public override uint Id { get; set; } = 44;
         public override string Name { get; set; } = "Prototype Keycard Basic";
 
@@ -66,8 +73,78 @@ namespace SnivysUltimatePackageOneConfig.Custom.Items.Keycards
                 },
             }
         };
+
+        [CanBeNull]
+        public static string KeycardName { get; set; } = "Prototype Keycard Basic";
+        public static KeycardLevels KeycardPermissions { get; set; } = new KeycardLevels(1, 0, 0);
+        public static Color32 KeycardPermissionsColor { get; set; } = new Color32(0, 0, 0, 255);
+        public static Color32 KeycardPrimaryColor { get; set; } = new Color32(255, 0, 255, 255);
+        public static string KeycardLabel { get; set; } = "Prototype Keycard Basic";
+        public static Color32 KeycardLabelColor { get; set; } = new Color32(255, 255, 255, 255);
+        
         public uint RefinedKeycardId { get; set; } = 45;
 
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+
+            Exiled.Events.Handlers.Map.PickupAdded += OnPickupAdded;
+            Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
+        }
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+
+            Exiled.Events.Handlers.Map.PickupAdded -= OnPickupAdded;
+            Exiled.Events.Handlers.Player.ItemAdded -= OnItemAdded;
+        }
+
+        private void OnPickupAdded(PickupAddedEventArgs ev) 
+        {
+            if (!Check(ev.Pickup))
+                return;
+
+            UpdateCard(ev.Pickup); 
+        }
+
+        private void OnItemAdded(ItemAddedEventArgs ev)
+        {
+            if (!Check(ev.Pickup))
+                return;
+
+            UpdateCard(ev.Pickup);
+        }
+        
+        private void UpdateCard(Pickup pickup) //code taken from KeycardItem (kinda)
+        {
+            if (!Type.TryGetTemplate<KeycardItem>(out var item))
+                throw new ArgumentException("Template for itemType not found");
+
+            if (!item.Customizable)
+                return;
+
+            int num = 0;
+            DetailBase[] details = item.Details;
+
+            object[] args = new object[]
+            {
+                KeycardName,
+                KeycardPermissions,
+                KeycardPermissionsColor,
+                KeycardPrimaryColor,
+                KeycardLabel,
+                KeycardLabelColor
+            };
+
+            for (int i = 0; i < details.Length; i++)
+            {
+                if (details[i] is ICustomizableDetail customizableDetail)
+                {
+                    customizableDetail.SetArguments(new ArraySegment<object>(args, num, customizableDetail.CustomizablePropertiesAmount));
+                    num += customizableDetail.CustomizablePropertiesAmount;
+                }
+            }
+        }
         protected override void OnUpgrading(UpgradingEventArgs ev)
         {
             ev.IsAllowed = false;
