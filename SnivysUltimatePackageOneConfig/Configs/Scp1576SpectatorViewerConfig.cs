e983ff604e0e5d064629a71be7381db563926d9b
Snivy Films <120346554+SnivyFilms@users.noreply.github.com>
2025-07-26 22:55:22 -0400

    Delete SnivysUltimatePackageOneConfig directory

--- SnivysUltimatePackageOneConfig/Configs/Scp1576SpectatorViewerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SnivysUltimatePackageOneConfig.Configs
-{
-    public class Scp1576SpectatorViewerConfig
-    {
-        public bool IsEnabled { get; set; } = true;
-        public string Scp1576Text { get; set; } = "<size=24><align=left>Spectators: %spectators%. Time before next spawn wave: %timebeforespawnwave% seconds</align></size>";
-        public float Scp1576TextDuration { get; set; } = 15f;
-    }
-} 
