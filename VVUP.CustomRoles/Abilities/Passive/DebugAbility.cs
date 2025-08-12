ffb6aa9567ff8696bf133d88b06e60fe1a5a387b
Snivy Films <mill385@yahoo.com>
2025-08-12 13:42:42 -0400

    9.8.0

--- VVUP.CustomRoles/Abilities/Passive/DebugAbility.cs
+++ VVUP.CustomRoles/Abilities/Passive/DebugAbility.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Doors;
 using Exiled.CustomRoles.API.Features;
+using Exiled.Events.EventArgs.Player;
 using MEC;
 
 namespace VVUP.CustomRoles.Abilities.Passive
@@ -11,19 +13,27 @@ namespace VVUP.CustomRoles.Abilities.Passive
     {
         private static CoroutineHandle _debugCoroutine;
         
-        public override string Name { get; set; } = "Ability Remover";
+        public override string Name { get; set; } = "Debug Ability";
 
         public override string Description { get; set; } =
-            "Removes any random abilities a user might get for whatever reason";
+            "Used for debugging purposes. Logs player position every 0.5 seconds, along with some other stuff.";
         
         protected override void AbilityAdded(Player player)
         {
             _debugCoroutine = Timing.RunCoroutine(DebugTracking(player));
+            Exiled.Events.Handlers.Player.InteractingDoor += OnDoorInteract;
+            foreach (Door door in Door.List)
+            {
+                if (door == null)
+                    continue;
+                Log.Warn($"VVUP: {door.Name} - {door.Position} - {door.Zone}");
+            }
         }
 
         protected override void AbilityRemoved(Player player)
         {
             Timing.KillCoroutines(_debugCoroutine);
+            Exiled.Events.Handlers.Player.InteractingDoor -= OnDoorInteract;
         }
 
         private static IEnumerator<float> DebugTracking(Player player)
@@ -33,6 +43,12 @@ namespace VVUP.CustomRoles.Abilities.Passive
                 Log.Warn($"VVUP: {player.Position}");
                 yield return Timing.WaitForSeconds(0.5f);
             }
+        }
+
+        private void OnDoorInteract(InteractingDoorEventArgs ev)
+        {
+            if (ev.Door != null)
+                Log.Warn($"VVUP: {ev.Door.Name}");
         }
     }
 }
